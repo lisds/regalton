fd7fbab1418c4a8af99f824fa54b2d948a28eb5b
Matthew Brett <matthew.brett@gmail.com>
2024-02-07 14:34:02 +0000

    Update from template

---
+++ tests/q_01_cmp_galtons.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question q_01_cmp_galtons',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> cmp_galtons(combined, reconstructed)
+          Result!
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
